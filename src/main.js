340cf2cd71b1bfbe4f6112b7021fc7f5cdcf1ad6
Pavel Sysoev <sysoevpvl@gmail.com>
2024-12-05 17:59:44 +0300

    adaptive hero, about,values,fullscreenmenu - completed

--- src/main.js
+++ src/main.js
@@ -4,3 +4,42 @@ if (process.env.NODE_ENV === "development") {
 }
 
 import "./scripts/skills";
+
+document.querySelector('.hamburger-menu').addEventListener('click', addFullScreenMenu);
+
+function addFullScreenMenu() {
+  let menu = document.querySelector('.full-screen-menu');
+  let closeBtn = menu.querySelector('.full-screen-menu__close');
+  let links = menu.querySelectorAll('.menu__link');
+
+  openFullScreenMenu();
+
+  closeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    closeFullScreenMenu()
+  })
+
+  links.forEach(item => {
+    item.addEventListener('click', (e) => {
+      closeFullScreenMenu()
+    })
+  })
+
+  function openFullScreenMenu() {
+    menu.classList.add('full-screen-menu--active');
+    document.body.style.overflow = "hidden";
+    document.addEventListener('click', checkClickOutsideFullScreenMenu);
+  }
+
+  function closeFullScreenMenu() {
+    document.querySelector('.full-screen-menu').classList.remove('full-screen-menu--active');
+    document.body.style.overflow = "";
+    document.removeEventListener('click', checkClickOutsideFullScreenMenu);
+  }
+
+  function checkClickOutsideFullScreenMenu() {
+    if (!event.target.closest('.full-screen-menu') && !event.target.closest('.hamburger-menu')) {
+      closeFullScreenMenu();
+    }
+  }
+}
